967f0e54d24f618347bcd85a2da2d75526dad303
Yiqiuhan <1456838931@qq.com>
2019-02-12 17:36:36 +0800

    first altohgrim

--- Automatic installation.py
+++ Automatic installation.py
@@ -1,1 +1,26 @@
-def
+import os
+path = "//panda/BRE_MASTERS_MFG/INV/R24-Senna/px64"
+generallist = []
+qalist = []
+for name in os.listdir(path):
+ generallist.append(name)
+ generallist.reverse()
+ a = (generallist[0])
+ 
+ if 'srv' not in a and 'dev' not in a:
+   qalist.append(a)
+   
+   qalist.reverse()
+   b = (qalist[0])
+    
+ else:
+  print ('c')
+   
+print (qalist)
+ 
+
+
+
+   
+
+ 
