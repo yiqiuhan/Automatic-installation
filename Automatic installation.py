06fd46e7f3058628ed4fbec33eb56628faae8793
Yiqiuhan <1456838931@qq.com>
2019-02-13 12:14:59 +0800

    classsify

--- Automatic installation.py
+++ Automatic installation.py
@@ -1,22 +1,30 @@
 import os
-path = "//panda/BRE_MASTERS_MFG/INV/R24-Senna/px64"
-generallist = []
-qalist = []
-for name in os.listdir(path):
- generallist.append(name)
- generallist.reverse()
- a = (generallist[0])
- 
- if 'srv' not in a and 'dev' not in a:
-   qalist.append(a)
-   
-   qalist.reverse()
-   b = (qalist[0])
-    
- else:
-  print ('c')
-   
-print (qalist)
+
+class versioncampare:
+ def webversion (self,path):
+  #path = "//panda/BRE_MASTERS_MFG/INV/R24-Senna/px64"
+  generallist = []
+  qalist = []
+  for name in os.listdir(path) :
+   generallist.append(name)
+   generallist.reverse()
+   a = (generallist[0])
+
+  #the last RTM check can be removed to exclude RTM versions
+   if 'srv' not in a and 'dev' not in a and 'QA' in a :
+    qalist.append(a)
+
+  b = (max(qalist))
+    #qalist.reverse()
+    #b = (qalist[0])
+  print (b)
+
+
+  def localversion (self):
+   return b
+c = versioncampare() 
+c.webversion("//panda/BRE_MASTERS_MFG/INV/R24-Senna/px64")
+
  
 
 
