85233100bf477dadc5931b211e0f960be50a0786
Yiqiuhan <1456838931@qq.com>
2019-02-21 14:46:36 +0800

    breakthrough the workflow, attention need admin theory

--- Automatic installation.py
+++ Automatic installation.py
@@ -1,6 +1,6 @@
 import os
 from winreg import *
-
+from subprocess import Popen
 
 class versioncampare:
  def webversion (self,path):
@@ -17,9 +17,10 @@ class versioncampare:
           qalist.append(a)
 
      b = (max(qalist))
-     c = b[4:7]
+     global d
+     d = b[4:7]
      global web 
-     web = int(c)
+     web = int(d)
      print (web)
      print(type(web))
 
@@ -38,18 +39,39 @@ class versioncampare:
          print ('you have not installed Inventor at local') 
 
  def campare(self, ww, ll):
-     if ww < ll:
+     if ww < ll or ww == ll:
          print ('you have already installed latest version ', local)
      else:
          print ('the web version is',web, 'which is higher than your local version')
-  
+         f = open("C:/Users/uninstall.bat", "w")
+
+         f.write( "Msiexec.exe /X {7F4DD591-2464-0001-0000-7107D70F3DB4} /passive\nMsiexec.exe /X {28B89EEF-3028-0409-0100-CF3F3A09B77D} /passive\nMsiexec.exe /X {B9312A51-41B5-479D-9F72-E7448A2D89AF} /passive\nMsiexec.exe /X {B46DECD1-2464-4EF1-0000-22D71E81877C} /passive\nMsiexec.exe /X {7F4DD591-2464-0001-1033-7107D70F3DB4} /passive" )
+
+         f = open("C:/Users/uninstall.bat", "a+")
+
+         f.write("\n\\\\beaver\BRE_MASTERS_MFG\INV\R24-Senna\px64\M24_" )
+
+         f.write( d )
+
+         f.write( "_x64_QA\Setup\Setup.exe /qb /LANG en-US /c INSTALLDIR=\"C:\Program Files\Autodesk\Inventor 2020\\\" ACADSERIALPREFIX=399 ACADSERIALNUMBER=99999966 ACADSTANDALONENETWORKTYPE=3 ACADSERVERPATH=\"CADQASERVER2 000000000000\" ACADLICENSESERVERTYPE=\"Single Server License\" ACADLICENSETYPE=\"Network License\" ACADFIRSTNAME=autodesk ACADLASTNAME=inc  ADLM_CONFIG_FILE=InventorCombinedConfig.pit ADLM_DEF_PRODKEY=797L1 PRODUCTEDITION=INVPROSA ADSK_EULA_STATUS=#1 ADSK_SOURCE_ROOT=\"M:\" FILESINUSETEXT="" REBOOT=ReallySuppress ADSK_SETUP_EXE=1 " )
+
+         # 关闭打开的文件
+         f.close()
+
+         p = Popen("uninstall.bat", cwd=r"C:/Users")
+         stdout, stderr = p.communicate()
    
 c = versioncampare() 
-c.webversion("//panda/BRE_MASTERS_MFG/INV/R24-Senna/px64")
+c.webversion("//beaver/BRE_MASTERS_MFG/INV/R24-Senna/px64")
 c.localversion(r'Software\Autodesk\Inventor\RegistryVersion24.0\System')
 c.campare(web,local)
 
- 
+
+
+
+
+
+
 
 
 
