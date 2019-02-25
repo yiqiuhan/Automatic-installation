97ccd150747d931b6d17809b0809eeb917a8517a
Yiqiuhan <1456838931@qq.com>
2019-02-25 16:13:37 +0800

    update process killing method

--- end task manager process.py
+++ end task manager process.py
@@ -1,3 +1,13 @@
 
 import os
-os.system('taskkill /f /im %s' % 'TeamViewer.exe')
+os.system('taskkill /f /im %s' % 'InvRO.exe')
+os.system('taskkill /f /im %s' % 'acad.exe')
+
+#import psutil, os
+#pids = psutil.pids()
+#for pid in pids:
+#        p = psutil.Process(pid)
+#        print('pid-%s,pname-%s' % (pid, p.name()))
+#        if p.name() == 'dllhost.exe':
+#            cmd = 'taskkill /F /IM dllhost.exe'
+#            os.system(cmd)
