85bbde4f34cc168ed7a6a6dcd9d8dee06de6e21c
Yiqiuhan <1456838931@qq.com>
2019-03-01 16:46:22 +0800

    fix bug:forget to import time and insert cmd exexution method for 5 times

--- automatic installation  with cmd.py
+++ automatic installation  with cmd.py
@@ -2,6 +2,7 @@ import os
 from winreg import *
 from subprocess import Popen
 import subprocess
+import time
 
 class versioncampare:
  def webversion (self,path):
@@ -52,10 +53,20 @@ class versioncampare:
          print (installcmd)
 
          cmd = "Msiexec.exe /X {7F4DD591-2464-0001-0000-7107D70F3DB4} /passive"
+         p = subprocess.Popen(cmd, shell=True)
+         p.wait()
          cmd = "Msiexec.exe /X {28B89EEF-3028-0409-0100-CF3F3A09B77D} /passive"
+         p = subprocess.Popen(cmd, shell=True)
+         p.wait()
          cmd = "Msiexec.exe /X {B9312A51-41B5-479D-9F72-E7448A2D89AF} /passive"
+         p = subprocess.Popen(cmd, shell=True)
+         p.wait()
          cmd = "Msiexec.exe /X {B46DECD1-2464-4EF1-0000-22D71E81877C} /passive"
+         p = subprocess.Popen(cmd, shell=True)
+         p.wait()
          cmd = "Msiexec.exe /X {7F4DD591-2464-0001-1033-7107D70F3DB4} /passive"
+         p = subprocess.Popen(cmd, shell=True)
+         p.wait()
 
          cmd = installcmd
 
